27f322815393f6aad85f6fb500b06dc26c093e88
Chavz24 <miguelchavezh@hotmail.com>
2021-10-15 17:24:41 -0400

    blog app finished

--- blog_app/blog_app/blog.py
+++ blog_app/blog_app/blog.py
@@ -9,6 +9,7 @@ from flask import url_for
 from flask import g
 import sqlite3
 import keys
+from functools import wraps
 
 
 # configuration
@@ -32,6 +33,22 @@ def connect_db():
     return sqlite3.connect(app.config["DATABASE"])
 
 
+# this function protects the man account from
+# being acceced if the user is not logged in.
+
+
+def loggin_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if "logged in" in session:
+            return test(*args, **kwargs)
+        else:
+            flash("You need to log in frist")
+            return redirect(url_for("login"))
+
+    return wrap
+
+
 @app.route("/", methods=["GET", "POST"])
 def login():
     error = None
@@ -52,8 +69,45 @@ def login():
 
 
 @app.route("/main")
+@loggin_required
 def main():
-    return render_template("main.html")
+
+    # connecting to the database
+    g.db = connect_db()
+
+    # fetching data from the table posts
+    cursor = g.db.execute("SELECT * FROM posts")
+
+    # creating a list of dicts that contain the data retrieved from the table
+    posts = [dict(title=row[0], post=row[1]) for row in cursor.fetchall()]
+
+    g.db.close()
+    return render_template("main.html", posts=posts)
+
+
+@app.route("/add", methods=["POST"])
+@loggin_required
+def add():
+    title = request.form["title"]
+    post = request.form["post"]
+
+    # the user has to provide a title and a post
+    if not title or not post:
+        flash("All fields are required. Please try again")
+        return redirect(url_for("main"))
+    else:
+        g.db = connect_db()
+        g.db.execute(
+            """
+            INSERT INTO posts(title,post) VALUES(?,?)
+            """,
+            [title, post],
+        )
+        g.db.commit()
+        g.db.close()
+        flash("New entry was successfully added.")
+
+    return redirect(url_for("main"))
 
 
 @app.route("/logout")
--- blog_app/blog_app/sql.py
+++ blog_app/blog_app/sql.py
@@ -6,20 +6,15 @@ with sqlite3.connect("blog.db") as conn:
 
     cursor = conn.cursor()
 
-    # create a table callle posts with two collumns tittle and post
-    cursor.executescript(
-        """
-        DROP TABLE IF EXISTS posts;
-        CREATE TABLE posts
-        (tittle TEXT, post TEXT)
-        """)
+    # create a table called posts with two columns tittle and post
+    cursor.execute(" CREATE TABLE IF NOT EXISTS posts(title TEXT, post TEXT)")
 
     # insert dummy data in it
-    dummy_data = [("Goog", "I'm okey."), ("Bad", "You're bad")]
-    cursor.executemany(
-        """
-        INSERT INTO posts(tittle, post)
-        VALUES(?,?)
-        """,
-        dummy_data,
-    )
+    # dummy_data = [("Goog", "I'm okey."), ("Bad", "You're bad")]
+    # cursor.executemany(
+    #     """
+    #     INSERT INTO posts(tittle, post)
+    #     VALUES(?,?)
+    #     """,
+    #     dummy_data,
+    # )
